9da1d65602fc10260fd6ba0786b5a4ce95e6f792
PBL075 <pavelgorea75@gmail.com>
2026-05-12 16:12:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace array_test2
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            int[] n = {1 , 2 , 3 , 4};
+
+            
+            foreach (int i in n)
+            Console.WriteLine(" El " + i);
+        }
+    }
+}
